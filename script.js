0574ed1d5445f6637c231a85b1982948a723409b
Roman Veselovsky <romanveselovsky@gmail.com>
2017-07-26 00:22:53 +0300

    HW12 JS Forms and regExp: fix style code

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-document.forms[0].onsubmit = function(e) {
-    e.preventDefault();
-
+document.forms[0].onsubmit = function ( event ) {
+    event.preventDefault();
     verifyUser();
 };
 
 function getLogin() {
     this.username = document.getElementsByClassName('с-form__username')[0].value;
     this.password = document.getElementsByClassName('с-form__password')[0].value;
-    var user = { username: username, password: password};
+    var user = {
+        username: username,
+        password: password
+    };
     this.obj = JSON.stringify(user);
 }
 
 function verifyUser() {
     getLogin();
 
-    var regExp1 = /^[a-z0-9]{2,15}$/i;
-    var regExp2 = /^(?=.*[#])(?=.*[$])[a-z0-9#$]{6,15}$/i;
-    var valid1 = regExp1.test(this.username);
-    var valid2 = regExp2.test(this.password);
+    var regExp1 = /^[a-z0-9]{2,15}$/i,
+        regExp2 = /^(?=.*[#])(?=.*[$])[a-z0-9#$]{6,15}$/i,
+        valid1 = regExp1.test(this.username),
+        valid2 = regExp2.test(this.password);
 
     if (valid1 === true && valid2 === true) {
-        entryinStorage();
+        entryInStorage();
     } else {
         console.log('The username or password you entered is incorrect');
     }
-
 }
 
-function entryinStorage() {
+function entryInStorage() {
     getLogin();
 
-    if (typeof(localStorage) !== 'undefined') {
+    if (typeof (localStorage) !== 'undefined') {
         localStorage.setItem('user', this.obj);
     } else {
-        setCookie('user', this.obj);
+        setCookie( 'user', this.obj );
     }
-
 }
 
-function setCookie(name, value, options) {
-    var expires = options.expires;
-    var updatedCookie = name + "=" + value;
-    var propValue;
-    var propName;
-    var d;
+function setCookie( name, value, options ) {
+    var expires = options.expires,
+        updatedCookie = name + "=" + value,
+        propValue,
+        propName,
+        date;
 
     options = options || {};
     value = encodeURIComponent(value);
 
     if (typeof expires == "number" && expires) {
-        d = new Date();
-        d.setTime(d.getTime() + expires * 1000);
-        expires = options.expires = d;
+        date = new Date();
+        date.setTime(date.getTime() + expires * 1000);
+        expires = options.expires = date;
     }
 
     if (expires && expires.toUTCString) {
@@ -61,6 +61,7 @@ function setCookie(name, value, options) {
     for (propName in options) {
         updatedCookie += "; " + propName;
         propValue = options[propName];
+
         if (propValue !== true) {
             updatedCookie += "=" + propValue;
         }
